42083d052ec9f5ff16c346640a84f43b4a642420
root <root@guest.guest>
2021-01-31 17:05:51 +0000

    Added serve

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -xe
 if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add
+  npm install -g serve
   npm run build
   sudo serve -s build -l 80
   rsync -rq --delete --rsync-path="mkdir -p react-app && rsync" \
