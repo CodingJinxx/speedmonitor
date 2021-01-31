a386c1f3b2308ec57e238a3d09585d5c17fdcdce
root <root@guest.guest>
2021-01-31 16:22:07 +0000

    Added Deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+
+if [ $TRAVIS_BRANCH == 'main' ] ; then
+  eval "$(ssh-agent -s)"
+  ssh-add
+  npm run build
+  serve -s build -l 80
+  rsync -rq --delete --rsync-path="mkdir -p react-app && rsync" \
+  $TRAVIS_BUILD_DIR/public travis@<ip>:react-app
+else
+  echo "Not deploying, since this branch isn't master."
+fi
